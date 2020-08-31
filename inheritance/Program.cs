acad5726c47a15d54b02467f4a27d3b6f1246f9c
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-31 18:48:35 +0530

    Add project files.

---
+++ inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+
+    class Employee
+    {
+         public string FirstName;
+        public string LastName;
+        string Email;
+
+        public void PrintFullName()
+        {
+
+        }
+
+    }
+
+    class FullTimeEmployee : Employee
+    {
+        public float AnnualSalary;
+    }
+
+    class PartTimeEmployee : Employee
+    {
+        float HourlySalary;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FullTimeEmployee FTE = new FullTimeEmployee();
+            FTE.FirstName = "Darshana";
+            FTE.LastName = "Senevirathna";
+            FTE.AnnualSalary = 120000;
+            FTE.PrintFullName();
+        }
+    }
+}
